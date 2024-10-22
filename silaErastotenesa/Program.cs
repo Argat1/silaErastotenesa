3a37df9ec7cef66c4e61c884f7a2030ad82f5671
lukasz.jablonski <lukasz.jablonski@S50-14>
2024-10-22 10:41:29 +0200

    added sito

--- silaErastotenesa/Program.cs
+++ silaErastotenesa/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace silaErastotenesa
+﻿using System;
+
+namespace silaErastotenesa
 {
     internal class Program
     {
+       void sito(int n)
+        {
+            bool[] tablica = new bool[100];
+
+            for(int i = 2; i <= tablica.Length; i++)
+            {
+                if (tablica[i] = true)
+                {
+                    for(int j = i*2; j<i ; j)
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+
+
     }
 }
